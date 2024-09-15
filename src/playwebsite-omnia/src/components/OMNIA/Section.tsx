bb65da35d2a58e399551149b099328049cf8e296
AndiappanAR <andiappan.ravi@omnia.ae>
2024-09-16 00:20:29 +0400

    custom code removed

--- src/playwebsite-omnia/src/components/OMNIA/Section.tsx
+++
@@ -1,7 +0,0 @@
-import { withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
-
-const HeroSectionCta = ({}): JSX.Element => {
-  return <></>;
-};
-
-export const Default = withDatasourceCheck()(HeroSectionCta);
